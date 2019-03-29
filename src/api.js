1477fadbfb7a37e9ef69d3f007fc3ec024e718b1
Owen Conti <owen@ohseemedia.com>
2019-03-29 17:33:04 -0600

    Remove double slash

--- src/api.js
+++ src/api.js
@@ -12,6 +12,6 @@ router.get("/", (req, res) => {
   });
 });
 
-app.use(`${isProduction ? "/.netlify/functions/" : ""}/api`, router);
+app.use(`${isProduction ? "/.netlify/functions" : ""}/api`, router);
 
 exports.handler = servless(app);

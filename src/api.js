2e02712d7f3b954c1fff6df1720f0c30679dc72e
Samim <samimhakimi11@gmail.com>
2024-03-12 10:13:18 +0000

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const os = require('os');
+const si = require('systeminformation');
+
+
 
 const app = express();
 const router = express.Router();
@@ -9,6 +13,35 @@ router.get("/", (req, res) => {
     hello: "hi!"
   });
 });
+
+router.get("/sysinfo", (req, res) => {
+  res.json({
+
+// Get the serial number of the machine
+const serialNumber = os.cpus()[0].serial;
+
+// Display the serial number to the user
+console.log(`Your serial number is: ${serialNumber}`);
+
+  });
+});
+
+
+router.get("/sysinformation", (req, res) => {
+  res.json({
+// Get the serial number of the machine
+     si.system().then(data => {
+    const serialNumber = data.serial;
+    // Display the serial number to the user
+    console.log(`Your serial number is: ${serialNumber}`);
+}).catch(error => {
+    console.error(`Error retrieving serial number: ${error}`);
+});
+
+
+  });
+});
+
 
 app.use(`/.netlify/functions/api`, router);
 
